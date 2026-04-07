623acc53ffe4c250b76b929eb2cdfee57db897fb
rbakhtiar <RahimBakhtair1122@gmail.com>
2026-04-07 16:38:00 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,41 @@
-self.addEventListener("install", e => {
-  console.log("Service Worker Installed");
+const CACHE_NAME = "iqmaster-cache-v1";
+const urlsToCache = [
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {});
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request);
+    })
+  );
+});
