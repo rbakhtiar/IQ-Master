12d539d6349b9e2ee30f4cc608e0e8a2d2da8766
rbakhtiar <RahimBakhtair1122@gmail.com>
2026-04-08 19:23:48 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,50 +1,28 @@
 const CACHE_NAME = 'iq-master-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)).then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) return response;
-        return fetch(event.request).then(fetchResponse => {
-          if (event.request.method !== 'GET' || !event.request.url.startsWith(self.location.origin)) {
-            return fetchResponse;
-          }
-          const responseClone = fetchResponse.clone();
-          caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request, responseClone);
-          });
-          return fetchResponse;
-        });
-      })
-      .catch(() => {
-        return caches.match('./index.html');
-      })
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).then(fetchResponse => {
+        if (event.request.method !== 'GET' || !event.request.url.startsWith(self.location.origin)) return fetchResponse;
+        const clone = fetchResponse.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return fetchResponse;
+      });
+    }).catch(() => caches.match('./index.html'))
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))).then(() => self.clients.claim())
   );
 });
