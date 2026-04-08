02e03abe5625e59870c7eea48e21b37129d35647
rbakhtiar <RahimBakhtair1122@gmail.com>
2026-04-08 18:42:25 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,50 @@
-const CACHE = "iqmaster-v2";
+const CACHE_NAME = 'iq-master-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll([
-      "./",
-      "./index.html",
-      "./manifest.json"
-    ]))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request).then(fetchResponse => {
+          if (event.request.method !== 'GET' || !event.request.url.startsWith(self.location.origin)) {
+            return fetchResponse;
+          }
+          const responseClone = fetchResponse.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+          return fetchResponse;
+        });
+      })
+      .catch(() => {
+        return caches.match('./index.html');
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
