6c4b631bd30a0b9a2c5664efb23849364971d5f0
rbakhtiar <RahimBakhtair1122@gmail.com>
2026-04-08 19:43:46 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,27 +2,27 @@ const CACHE_NAME = 'iq-master-v1';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)).then(() => self.skipWaiting())
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)).then(() => self.skipWaiting())
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) return response;
-      return fetch(event.request).then(fetchResponse => {
-        if (event.request.method !== 'GET' || !event.request.url.startsWith(self.location.origin)) return fetchResponse;
-        const clone = fetchResponse.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-        return fetchResponse;
-      });
-    }).catch(() => caches.match('./index.html'))
-  );
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            if (response) return response;
+            return fetch(event.request).then(fetchResponse => {
+                if (event.request.method !== 'GET' || !event.request.url.startsWith(self.location.origin)) return fetchResponse;
+                const clone = fetchResponse.clone();
+                caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+                return fetchResponse;
+            });
+        }).catch(() => caches.match('./index.html'))
+    );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))).then(() => self.clients.claim())
-  );
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))).then(() => self.clients.claim())
+    );
 });
